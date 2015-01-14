fede1071a7c3f8410d07598e26e4b66f8e8b723f
Sam <sam.fiechter@gmail.com>
2015-01-13 21:19:03 -0500

    Tue Jan 13 21:19:03 EST 2015

--- emacs-js.el
+++ emacs-js.el
@@ -11,28 +11,47 @@
 ;;;Code
 ;; ;;;;;;;;;;;;; variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar emacs-js-expr (list '( emacs-js-array
-			       emacs-js-obj
-			       emacs-js-name
-			       emacs-js-numeric
-			       emacs-js-string
-			       emacs-js-prototype
-			       emacs-js-math
-			       emacs-js-function)))
-(defvar emacs-js-array '( "[" ( emacs-js-expr ( emacs-js-zero-or-more "," emacs-js-expr)) "]"))
-(defvar emacs-js-obj-expr '( emacs-js-name ":" emacs-js-expr))
-(defvar emacs-js-obj '( "{" ( emacs-js-obj-expr  ( emacs-js-zero-or-more "," emacs-js-obj-expr)) "}"))
-(defvar emacs-js-numeric "^\\([-+]?[:space:]*[0-9]+\\(\.[0-9]+\\)?\\([eE][+-]?[0-9]+\\)\\|0x[:xdigit:]+\\)?")
-(defvar emacs-js-name "^[A-Za-z\$\_0-9][^[:space:]{}().]*")		
+(defvar emacs-js-expr (cons  '( (list
+				 emacs-js-array
+				 emacs-js-obj
+				 emacs-js-name
+				 emacs-js-numeric
+				 ;;			       emacs-js-string
+				 ;;			       emacs-js-prototype
+				 ;;			       emacs-js-math
+				 ;;			       emacs-js-function
+				 ))
+			     (lambda (l) l)))
 
+(defvar emacs-js-array-more (cons '( "," emacs-js-expr) (lambda (l) (elt l 1)))) 
+(defvar emacs-js-array (cons '( "\[" ( emacs-js-expr (list "\]" emacs-js-array-more )))
+			     (lambda (l) (let ((i 0) (n (- (length l) 1)) (a (array))) (while (i < n ) (apush a (elt l i)) (setq i (+ 2 i))) a))))
 
-(defun ejs-eval (jstext) "evaluate javascript text"
+(defvar emacs-js-obj-expr (cons '( emacs-js-name ":" emacs-js-expr)
+				(lambda (l) (list (elt l 0) (elt l 2)))))
+(defvar emacs-js-obj-more  (cons '( "," emacs-js-obj-expr) (lambda (l) (list (elt l 1)))))
+
+(defvar emacs-js-obj (cons '( "\{" ( emacs-js-obj-expr  (list "\}" emacs-js-obj-more)))
+							"," emacs-js-obj-expr)) "}")
+			   (lambda (l) (let ((i 0) (n (- (length l) 1)) (h (makehash))) (while (i < n) (puthash (elt l i) (elt l (+ 1 i)) h) (setq i (+ 2 i))) h))))
+(defvar emacs-js-numeric (cons "\\([-+]?[:space:]*[0-9]+\\(\.[0-9]+\\)?\\([eE][+-]?[0-9]+\\)\\|0x[:xdigit:]+\\)?"
+			       (lambda (l) (string-to-number (l)))))
+
+(defvar emacs-js-name (cons "[a-zA-Z\$_][^\s]*") (lambda (l) l))
+(defvar emacs-js-defvar (cons '( "var" emacs-js-name "=" emacs-js-expr ";") (lambda (l) (defvar (elt l 1) (elt l 3))))) 
+
+(defun emacs-js-testp (jstext var)
+
+  )
+
+(defun emacs-js-eval (jstext) "evaluate javascript text"
     (let* (
 	   
 	   )
       )
   )
-
+(defmacro apush (arr var)
+  (aset arr (length arr) var))
 (defmacro inc (var)
   (list 'setq var (list '1+ var)))
 
@@ -55,10 +74,9 @@
 	   (skip-chars-not-listed cend jstext len 
 	   
 	   )
-	 ))
+)))
 
-(defun emacs-js-evalt
-    )
+
 
 (defun char-in-sstr (c s)
   (let ((i 0)
